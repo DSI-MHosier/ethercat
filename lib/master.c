980d2447857d10b55085c33be02944478f5b2d44
Michael Hosier <michael.hosier@gleeble.com>
2025-10-28 15:34:48 -0400

    undoing some of "Applied 0007-replace-fprintf-calls-with-EC_PRINT_ERR.patch" from 6cac716122a74352c306e55209c4f0a42077436d

--- lib/master.c
+++ lib/master.c
@@ -786,7 +786,7 @@ void ecrt_master_64bit_reference_clock_time_queue(ec_master_t *master)
     // we only report the first error of its kind, otherwise the errors
     // will flood the logs
     if ( (ret != lastErr64BitRefClkQueue) && EC_IOCTL_IS_ERROR(ret) ) {
-        EC_PRINT_ERR("Failed to queue 64bit ref clock time datagram: %s\n",
+        fprintf(stderr, "Failed to queue 64bit ref clock time datagram: %s\n",
                 strerror(EC_IOCTL_ERRNO(ret)));
     }
     lastErr64BitRefClkQueue = ret;
@@ -805,7 +805,7 @@ int ecrt_master_64bit_reference_clock_time(ec_master_t *master,
     // will flood the logs
     if ( (ret != lastErr64BitRefClk) && EC_IOCTL_IS_ERROR(ret) && 
          (EC_IOCTL_ERRNO(ret) != EAGAIN) ) {
-        EC_PRINT_ERR("Failed to get 64bit reference clock time: %s\n",
+        fprintf(stderr, "Failed to get 64bit reference clock time: %s\n",
                 strerror(EC_IOCTL_ERRNO(ret)));
     }
     lastErr64BitRefClk = ret;
